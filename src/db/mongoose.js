3c75dcca622097262557f2526a3aa7dea2cad99d
RU$HABH <rnpatel2226@gmail.com>
2020-07-18 19:40:26 +0530

    Initial commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+// Connecting with Database
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
