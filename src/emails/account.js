5d3c2f16113f9e767c1b8451fea6b6f73b5c2709
Rushabh Patel <54587175+Rushabh-454@users.noreply.github.com>
2021-12-18 12:40:15 +0530

    Setting user email as environment variable

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'argv1232@gmail.com',
+        from: process.env.USER_EMAIL,
         subject: 'Thanks for joining in!',
         text: `Welcome to Task Manager App, ${name}. Let me know how you get along with the app :)`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'argv1232@gmail.com',
+        from: process.env.USER_EMAIL,
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. Hope to see you back sometime soon.`
     })
